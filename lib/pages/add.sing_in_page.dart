3b086668d3a45f4eb6bba06c22e7ea081331f566
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-05 18:29:56 +0200

    add ui to sing in page

---
+++ lib/pages/add.sing_in_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:wather_app/pages/search_city.dart';
+import 'package:wather_app/widgets/MyBatton.dart';
+import 'package:wather_app/widgets/text_field.dart';
+
+class SingIn extends StatefulWidget {
+  @override
+  State<SingIn> createState() => _SingInState();
+}
+
+class _SingInState extends State<SingIn> {
+   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: const Color(0xFFEAEAEA),
+      body: Form(
+        key: formKey,
+        child: Center(
+          
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            width: 350,
+            child:  Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+              const  Icon(Icons.water_drop_sharp , size: 50,  color: Colors.blue,),
+              const  SizedBox(height: 20),
+              const   Text(
+                  'Sign in to your account',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+               const  SizedBox(height: 20),
+                
+              const  MyButton(
+                  label: "Sign in with Google", 
+                  icon: Icons.g_mobiledata,
+                  ),  
+        
+                const  SizedBox(height: 10,),
+        
+                const  MyButton(
+                  label: "Sign in with Apple", 
+                  icon: Icons.apple,
+                  ),
+                
+                const SizedBox(height: 20),
+                const Divider(thickness: 1),
+                const SizedBox(height: 10),
+                const Text_From_Fiel(
+                  obscureText: false,
+                   hintext: "Email addres",
+                   ), 
+                const   SizedBox(height: 10,),
+                   const Text_From_Fiel(
+                  obscureText: true,
+                   hintext: "Password",
+                   ),
+                const SizedBox(height: 10),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Forgot password?'),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () {
+                    if(formKey.currentState!.validate()){
+                      Navigator.push(context, MaterialPageRoute(builder: (context){
+                      return SearchCity();
+                    }));
+                    }
+                    
+                  },
+                  child: const Text('Continue' , style: TextStyle(
+                    color: Colors.white
+                  ),),
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
